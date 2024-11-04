2949cd1792fa1067d72d8a7a4309967bb977709d
Usuario <Usuario@CPD03-M19>
2024-11-04 15:10:52 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+namespace MauiAppMinhasCompras.Models
+
+public class Produto
+{
+	string _descricao;
+	[PrimaryKey, AutoIncrement]
+	public int Id { get; set; }
+	public string Descricao
+	{
+		get => _descricao;
+		set
+		{
+			if (value == null)
+			{
+				throw new exception("por favor, preencha a descrição");
+			}
+			Descricao = value;
+		}
+	}
+}
+		
+
+
+	
+	
---
+++ SQLiteDatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+
+	public class SQLiteDatabaseHelper
+	{
+		readonly SQLiteDatabaseHelper
+		public SQLiteDatabaseHelper(string path)
+
+		{
+			_conn = new SQLiteDatabaseHelper(string path);
+			_conn.CreateTableAsync<Produto>().wait();
+
+		}
+	}
+}
